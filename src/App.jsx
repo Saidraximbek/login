015fa7fba1185f997b2324dddf426af462e37ca0
saidrxim <saidrxim@gmail.com>
2025-04-03 17:29:26 +0500

    initialize project structure with React, Vite, and Tailwind CSS; add essential components and hooks

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import ProtectedROutest from "./components/ProtectedROutest";
+import MainLayout from "./layouts/MainLayout";
+
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+
+import { useGlobalContext } from "./hooks/useGlobalContext";
+import { useEffect } from "react";
+import { auth } from "./firebase/config";
+import { onAuthStateChanged } from "firebase/auth";
+
+function App() {
+  const { user, dispatch, isAuthReady } = useGlobalContext();
+
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedROutest user={user}>
+          <MainLayout />
+        </ProtectedROutest>
+      ),
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/create",
+          element: <Create />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: user ? <Navigate to="/" /> : <Login />,
+    },
+    {
+      path: "/register",
+      element: user ? <Navigate to="/" /> : <Register />,
+    },
+  ]);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch({ type: "LOGIN", payload: user });
+      }
+      dispatch({ type: "AUTH_READY" });
+    });
+
+    return () => unsubscribe(); 
+  }, []);
+
+  return <>{isAuthReady && <RouterProvider router={routes} />}</>;
+}
+
+export default App;
